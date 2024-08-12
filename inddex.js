d9637a93a9dce226b144c45026b966f3d3d2a0f4
Harshbhatnagar12 <harshbhatnagar2022@gmail.com>
2024-08-12 23:02:58 +0530

    Faulty Calculator

---
+++ inddex.js
@@ -0,0 +1,30 @@
+
+
+
+let random = Math.random( )
+console.log(random)
+let a = prompt("Enter the  First Number")
+let c = prompt("Enter the opeartion")
+let b = prompt("Enter the Second Number")
+
+let obj = {
+    "+":"-",
+    "*":"+",
+    "-": "/",
+    "/":"**",
+}
+
+// They print right Result when result is less then 0.1
+if (random > 0.1)
+{
+    console.log(`The result is ${a} ${c} ${b}`)
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+}
+
+// They print wrong Result when the result is greater then 0.1 
+else
+{
+    c = obj[c]
+    alert(`The Main result is ${eval(`${a} ${c} ${b}`)}`)
+
+}
